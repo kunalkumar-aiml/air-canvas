2d3cdf241f6dcabfd303c43ab58949b3d80242fc
Kunal Kumar <kunalkumar.aiml@gmail.com>
2026-03-01 21:02:06 +0530

    Add configuration module

---
+++ config.py
@@ -0,0 +1,15 @@
+# Application configuration
+
+CAMERA_INDEX = 0
+
+BRUSH_THICKNESS = 6
+ERASER_THICKNESS = 40
+
+COLORS = {
+    "blue": (255, 0, 0),
+    "green": (0, 255, 0),
+    "red": (0, 0, 255),
+    "yellow": (0, 255, 255)
+}
+
+UI_HEIGHT = 60
