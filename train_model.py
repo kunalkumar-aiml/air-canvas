bd0d69accd4b9b7ade0421bdc052ea854745b448
Kunal Kumar <kunalkumar.aiml@gmail.com>
2026-03-05 23:58:18 +0530

    Add MNIST training pipeline
    Refactor import statements and format model fitting code for better readability.

--- train_model.py
+++ train_model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers
+from tensorflow.keras import models
 from tensorflow.keras.datasets import mnist
 
 
@@ -11,6 +12,7 @@ x_test = x_test / 255.0
 x_train = x_train.reshape(-1, 28, 28, 1)
 x_test = x_test.reshape(-1, 28, 28, 1)
 
+
 model = models.Sequential([
     layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
     layers.MaxPooling2D(2,2),
@@ -21,8 +23,10 @@ model = models.Sequential([
     layers.Flatten(),
 
     layers.Dense(128,activation='relu'),
+
     layers.Dense(10,activation='softmax')
 ])
+
 
 model.compile(
     optimizer='adam',
@@ -30,6 +34,11 @@ model.compile(
     metrics=['accuracy']
 )
 
-model.fit(x_train,y_train,epochs=5)
+
+model.fit(
+    x_train,
+    y_train,
+    epochs=5
+)
 
 model.save("digit_model.h5")
