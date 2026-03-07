e2912e9b47a8cdff4b4207e190a046b425d479d4
Kunal Kumar <kunalkumar.aiml@gmail.com>
2026-03-07 16:06:20 +0530

    Add finger gesture drawing and erase support

--- drawing.py
+++ drawing.py
@@ -6,60 +6,81 @@ import config
 
 class Drawer:
     def __init__(self):
+
         self.canvas = None
         self.prev_x = None
         self.prev_y = None
+
         self.color = config.COLORS["blue"]
+
+        self.brush_thickness = config.BRUSH_THICKNESS
+        self.eraser_thickness = config.ERASER_THICKNESS
+
         self.last_save_time = 0
-        self.erase_mode = False
 
     def initialize_canvas(self, frame):
+
         if self.canvas is None:
             self.canvas = np.zeros_like(frame)
 
-    def smooth_point(self, x, y, alpha=0.5):
-        if self.prev_x is None:
-            return x, y
-        smooth_x = int(self.prev_x * (1 - alpha) + x * alpha)
-        smooth_y = int(self.prev_y * (1 - alpha) + y * alpha)
-        return smooth_x, smooth_y
-
     def draw(self, x, y):
-        x, y = self.smooth_point(x, y)
 
         if self.prev_x is None:
             self.prev_x, self.prev_y = x, y
 
-        if self.erase_mode:
-            cv2.line(self.canvas, (self.prev_x, self.prev_y), (x, y),
-                     (0, 0, 0), config.ERASER_THICKNESS)
-        else:
-            cv2.line(self.canvas, (self.prev_x, self.prev_y), (x, y),
-                     self.color, config.BRUSH_THICKNESS)
+        cv2.line(
+            self.canvas,
+            (self.prev_x, self.prev_y),
+            (x, y),
+            self.color,
+            self.brush_thickness
+        )
 
-        self.prev_x, self.prev_y = x, y
+        self.prev_x = x
+        self.prev_y = y
+
+    def erase(self, x, y):
+
+        if self.prev_x is None:
+            self.prev_x, self.prev_y = x, y
+
+        cv2.line(
+            self.canvas,
+            (self.prev_x, self.prev_y),
+            (x, y),
+            (0, 0, 0),
+            self.eraser_thickness
+        )
+
+        self.prev_x = x
+        self.prev_y = y
 
     def reset_position(self):
+
         self.prev_x = None
         self.prev_y = None
 
     def clear_canvas(self):
+
         if self.canvas is not None:
             self.canvas[:] = 0
 
     def save_canvas(self):
+
         current_time = time.time()
+
         if current_time - self.last_save_time > 1:
+
             filename = f"drawing_{int(current_time)}.png"
+
             cv2.imwrite(filename, self.canvas)
+
             self.last_save_time = current_time
 
     def set_color(self, color):
-        self.erase_mode = False
+
         self.color = color
 
-    def enable_eraser(self):
-        self.erase_mode = True
-
     def get_output(self, frame):
+
         return cv2.add(frame, self.canvas)
