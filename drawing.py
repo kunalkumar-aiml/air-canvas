2fda5ba7f36c40708fcfefeafe426aa21c73daa4
Kunal Kumar <kunalkumar.aiml@gmail.com>
2026-03-07 16:20:55 +0530

    Add brush size and shape drawing support

--- drawing.py
+++ drawing.py
@@ -1,86 +1,80 @@
 import cv2
 import numpy as np
-import time
-import config
 
 
 class Drawer:
+
     def __init__(self):
 
         self.canvas = None
         self.prev_x = None
         self.prev_y = None
 
-        self.color = config.COLORS["blue"]
+        self.color = (255,0,0)
+        self.brush_size = 5
 
-        self.brush_thickness = config.BRUSH_THICKNESS
-        self.eraser_thickness = config.ERASER_THICKNESS
-
-        self.last_save_time = 0
+        self.shape_mode = None
 
     def initialize_canvas(self, frame):
 
         if self.canvas is None:
             self.canvas = np.zeros_like(frame)
 
-    def draw(self, x, y):
+    def draw(self,x,y):
 
         if self.prev_x is None:
-            self.prev_x, self.prev_y = x, y
+            self.prev_x,self.prev_y = x,y
 
         cv2.line(
             self.canvas,
-            (self.prev_x, self.prev_y),
-            (x, y),
+            (self.prev_x,self.prev_y),
+            (x,y),
             self.color,
-            self.brush_thickness
+            self.brush_size
         )
 
-        self.prev_x = x
-        self.prev_y = y
+        self.prev_x,self.prev_y = x,y
 
-    def erase(self, x, y):
+    def erase(self,x,y):
 
         if self.prev_x is None:
-            self.prev_x, self.prev_y = x, y
+            self.prev_x,self.prev_y = x,y
 
         cv2.line(
             self.canvas,
-            (self.prev_x, self.prev_y),
-            (x, y),
-            (0, 0, 0),
-            self.eraser_thickness
+            (self.prev_x,self.prev_y),
+            (x,y),
+            (0,0,0),
+            30
         )
 
-        self.prev_x = x
-        self.prev_y = y
+        self.prev_x,self.prev_y = x,y
 
-    def reset_position(self):
+    def reset(self):
 
         self.prev_x = None
         self.prev_y = None
 
-    def clear_canvas(self):
-
-        if self.canvas is not None:
-            self.canvas[:] = 0
-
-    def save_canvas(self):
-
-        current_time = time.time()
-
-        if current_time - self.last_save_time > 1:
-
-            filename = f"drawing_{int(current_time)}.png"
-
-            cv2.imwrite(filename, self.canvas)
-
-            self.last_save_time = current_time
-
-    def set_color(self, color):
+    def set_color(self,color):
 
         self.color = color
 
-    def get_output(self, frame):
+    def set_brush(self,size):
 
-        return cv2.add(frame, self.canvas)
+        self.brush_size = size
+
+    def draw_shape(self,x1,y1,x2,y2):
+
+        if self.shape_mode == "rectangle":
+
+            cv2.rectangle(self.canvas,(x1,y1),(x2,y2),self.color,3)
+
+        elif self.shape_mode == "circle":
+
+            radius = int(((x2-x1)**2 + (y2-y1)**2)**0.5)
+
+            cv2.circle(self.canvas,(x1,y1),radius,self.color,3)
+
+    def output(self,frame):
+
+        return cv2.add(frame,self.canvas)
