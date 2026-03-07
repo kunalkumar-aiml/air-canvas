3e8eeba99254af10a5f95ba09607de56149370af
Kunal Kumar <kunalkumar.aiml@gmail.com>
2026-03-07 16:20:14 +0530

    Add color palette and brush size config

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-# Application configuration
-
 CAMERA_INDEX = 0
 
-BRUSH_THICKNESS = 6
-ERASER_THICKNESS = 40
+BRUSH_SIZES = [3, 8, 15]
 
-COLORS = {
-    "blue": (255, 0, 0),
-    "green": (0, 255, 0),
-    "red": (0, 0, 255),
-    "yellow": (0, 255, 255)
-}
+COLORS = [
+    (255,0,0),
+    (0,255,0),
+    (0,0,255),
+    (255,255,0),
+    (255,0,255),
+    (0,255,255),
+    (255,255,255),
+]
 
-UI_HEIGHT = 60
+UI_HEIGHT = 80
